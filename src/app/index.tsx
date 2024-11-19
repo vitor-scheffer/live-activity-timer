c09e88872a69012fdbda2b32450442efc91a51ac
Vitor Scheffer <vitorscheffer61@gmail.com>
2024-11-19 20:06:14 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FFFFFF",
+      }}
+    >
+        <Text>Welcome to the Live Activity Timer App!</Text>
+    </View>
+  );
+}
